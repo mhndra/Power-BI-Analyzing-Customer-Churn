81825167dc9b695ccbbc598f3875e06800f4ef74
mhndra <m31778049@gmail.com>
2026-02-01 04:51:27 +0000

    Add ingestion query

--- snowflake/ingestion/copy_into.sql
+++ snowflake/ingestion/copy_into.sql
@@ -1,6 +1,19 @@
 USE ROLE sysadmin;
 
+USE WAREHOUSE customer_churn_wh;
+
+USE SCHEMA customer_churn_db.raw;
+
+
 /*
 Ingest data using COPY INTO <table>
 */
 
+LIST @customer_churn_stage;
+
+COPY INTO customer_churn_raw
+FROM @customer_churn_stage;
+
+SELECT * 
+FROM customer_churn_raw 
+LIMIT 5;
