0ab4aa831eba8433d5a622e4e09d2644cd9d6409
mhndra <m31778049@gmail.com>
2026-02-01 04:00:15 +0000

    Added DDL query

---
+++ snowflake/ddl/teardown_objects.sql
@@ -0,0 +1,17 @@
+USE ROLE sysadmin;
+
+
+/*
+Drop Database and Database Objects
+*/
+
+DROP DATABASE customer_churn_db;
+
+
+/*
+Drop Warehouse
+*/
+
+DROP WAREHOUSE customer_churn_wh;
+
+SHOW WAREHOUSES;
