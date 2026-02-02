7f911ef67fe80d298df6914f21322dd109bd8cdc
mhndra <m31778049@gmail.com>
2026-02-02 10:48:25 +0000

    Update indentation

--- snowflake/ddl/create_objects.sql
+++ snowflake/ddl/create_objects.sql
@@ -6,10 +6,10 @@ Create warehouse
 */
 
 CREATE OR ALTER WAREHOUSE customer_churn_wh
-  WAREHOUSE_TYPE = STANDARD
-  WAREHOUSE_SIZE = XSMALL
-  AUTO_RESUME = TRUE
-  AUTO_SUSPEND = 60
+    WAREHOUSE_TYPE = STANDARD
+    WAREHOUSE_SIZE = XSMALL
+    AUTO_RESUME = TRUE
+    AUTO_SUSPEND = 60
 ;
 
 SHOW WAREHOUSES LIKE 'customer_churn_wh';
@@ -39,12 +39,12 @@ Create stage
 
 -- Internal stage
 CREATE OR ALTER STAGE customer_churn_db.raw.customer_churn_stage
-  ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
-  DIRECTORY = (ENABLE = TRUE)
-  FILE_FORMAT = (
-    TYPE = CSV 
-    SKIP_HEADER = 1
-  )
+    ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE')
+    DIRECTORY = (ENABLE = TRUE)
+    FILE_FORMAT = (
+        TYPE = CSV 
+        SKIP_HEADER = 1
+    )
 ;
 
 DESCRIBE STAGE customer_churn_db.raw.customer_churn_stage;
@@ -56,32 +56,32 @@ Create table
 
 -- Transient table
 CREATE OR REPLACE TABLE customer_churn_db.marts.customer_churn_table (
-  customer_id STRING,
-  churned REAL,
-  account_length_months REAL,
-  local_calls REAL,
-  local_mins REAL,
-  intl_calls REAL,
-  intl_mins REAL,
-  intl_active STRING,
-  intl_plan STRING,
-  extra_international_charges REAL,
-  customer_service_calls REAL,
-  group_consumption STRING,
-  unimited_data_plan STRING,
-  extra_data_charge REAL,
-  state STRING,
-  phone_number STRING,
-  gender STRING,
-  age REAL,
-  demographics STRING,
-  part_group_contract STRING,
-  number_of_customers_in_group REAL,
-  device_protection_and_online_backup STRING,
-  contract_category STRING,
-  payment_method STRING,
-  monthly_charge REAL,
-  total_charges REAL,
-  churn_category STRING,
-  churn_reason STRING
+    customer_id STRING,
+    churned REAL,
+    account_length_months REAL,
+    local_calls REAL,
+    local_mins REAL,
+    intl_calls REAL,
+    intl_mins REAL,
+    intl_active STRING,
+    intl_plan STRING,
+    extra_international_charges REAL,
+    customer_service_calls REAL,
+    group_consumption STRING,
+    unimited_data_plan STRING,
+    extra_data_charge REAL,
+    state STRING,
+    phone_number STRING,
+    gender STRING,
+    age REAL,
+    demographics STRING,
+    part_group_contract STRING,
+    number_of_customers_in_group REAL,
+    device_protection_and_online_backup STRING,
+    contract_category STRING,
+    payment_method STRING,
+    monthly_charge REAL,
+    total_charges REAL,
+    churn_category STRING,
+    churn_reason STRING
 );
